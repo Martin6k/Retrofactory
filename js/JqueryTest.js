bee81bab7e61ffa509db8a8744bca7eb109120fe
Martin6k <134512513+Martin6k@users.noreply.github.com>
2024-05-10 23:02:55 -0400

    0.0.5.7
    Validacion de contraseña en la vista de Sesion

---
+++ js/JqueryTest.js
@@ -0,0 +1,17 @@
+
+/*
+function validar(password,email) {
+if (String(password).length < 9 || String(rut).length > 10) {
+    /* Validacion Rut */
+  /*  $("#check").html("<div class='alert alert-danger w-50 mx-auto text-center' >Rut debe tener largo entre 9 y 10 caracteres</div>");
+} else if (String(nombre).length < 3 || String(nombre).length > 20) {
+    /* Validacion Nombre */
+  /*  $("#check").html("<div class='alert alert-danger w-50 mx-auto text-center' >Nombre debe tener largo entre 3 y 20 caracteres</div>");
+} else {
+    /* Confirmacion */
+/*    return true;
+}
+
+}
+});
+*/
