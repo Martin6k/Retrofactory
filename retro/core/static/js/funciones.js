26e81eea3c8b5be63222f90f196ef237541d319a
Benjamín Díaz <benj.diazv@duocuc.cl>
2024-06-11 15:12:03 -0400

    0.7.3
    Se actualizaron todos los formularios para que hicieran la validación al perder el foco

--- retro/core/static/js/funciones.js
+++ retro/core/static/js/funciones.js
@@ -1,83 +1,71 @@
-function validacion_form(){
+function val_nom(){
     var nom = document.getElementById("nombre").value;
-    var rut = document.getElementById("rut").value;
-    var fono = document.getElementById("fono").value
-    var correo = document.getElementById("correo").value;
-    var edad = document.getElementById("edad").value;
-    var razon = document.getElementById("razon").value;
-    //validacion nombre
     if (String(nom).length > 0){
-        //validacion rut
-        if (String(rut).length >= 9 && String(rut).length <= 10) {
-            //validacion fono
-            if (String(fono).length >= 9 && String(fono).length <= 12) {
-                //validacion correo
-                if (String(correo).includes('@gmail.com')) {
-                    //validacion edad
-                    if (Number(edad) >= 18){
-                        //validar el fono
-                        if(Number(razon) >= 2 && Number(razon) <= 4){
-                            var res = document.getElementById("razon");
-                            res.style.border = "1px solid green"
-                            document.getElementById("resultado").innerHTML = "<div class='alert alert-success  mx-auto text-center'>" +
-                            "formulario completo</div>"
-                        }else{
-                            var res = document.getElementById("razon");
-                            res.style.border = "1px solid red"
-                            document.getElementById("resultado").innerHTML = "<div class='alert alert-danger  mx-auto text-center'>" +
-                                "Seleccione una opcion valida</div>"
-                        }
-                    }else{
-                        var res = document.getElementById("edad");
-                        res.style.border = "1px solid red"
-                        document.getElementById("resultado").innerHTML = "<div class='alert alert-danger  mx-auto text-center'>" +
-                            "debes ser mayor de edad</div>"
-                    }
-                }else {
-                    var res = document.getElementById("correo");
-                    res.style.border = "1px solid red"
-                    document.getElementById("resultado").innerHTML = "<div class='alert alert-danger  mx-auto text-center'>" +
-                        "El correo no es válido</div>"
-                }
-            }else {
-                var res = document.getElementById("fono");
-                res.style.border = "1px solid red"
-                document.getElementById("resultado").innerHTML = "<div class='alert alert-danger  mx-auto text-center'>" +
-                    "El teléfono no es válido</div>"
-            }
-        }else {
-            var res = document.getElementById("rut");
-            res.style.border = "1px solid red"
-            document.getElementById("resultado").innerHTML = "<div class='alert alert-danger mx-auto text-center'>" +
-                "El rut no es válido</div>"
-        }
-    }else {
+        document.getElementById("nombre").style.border= "5px solid green";
+    }else{
         var res = document.getElementById("nombre");
         res.style.border = "1px solid red"
         document.getElementById("resultado").innerHTML = "<div class='alert alert-danger  mx-auto text-center'>" +
             "El campo esta vacío</div>";
     }
 }
-
-function validacion_ini(){
-    var mail = document.getElementById("email").value;
-    var con = document.getElementById("pass").value;
-    if(String(mail).includes('@gmail.com')){
-        if(String(con).length >= 8 && String(con).length <= 15){
-            var res = document.getElementById("pass");
-            res.style.border = "1px solid green"
-            document.getElementById("resultado").innerHTML = "<div class='alert alert-success  mx-auto text-center'>" +
-            "Sesión iniciada</div>"
-        }else{
-            var res = document.getElementById("pass");
-            res.style.border = "1px solid red"
-            document.getElementById("resultado").innerHTML = "<div class='alert alert-danger  mx-auto text-center'>" +
-                "La contraseña debe tener entre 8 y 15 caracteres</div>";
-        }
+function val_rut(){
+    var rut = document.getElementById("rut").value;
+    if (String(rut).length >= 9 && String(rut).length <= 10){
+        document.getElementById("rut").style.border= "5px solid green";
     }else{
-        var res = document.getElementById("email");
+        var res = document.getElementById("rut");
+        res.style.border = "1px solid red"
+        document.getElementById("resultado").innerHTML = "<div class='alert alert-danger mx-auto text-center'>" +
+        "El rut no es válido</div>"
+    }
+    
+}
+function val_fono(){
+    var fono = document.getElementById("fono").value;
+    if (String(fono).length >= 9 && String(fono).length <= 12){
+        document.getElementById("fono").style.border= "5px solid green";
+    }else{
+        var res = document.getElementById("fono");
         res.style.border = "1px solid red"
         document.getElementById("resultado").innerHTML = "<div class='alert alert-danger  mx-auto text-center'>" +
-            "El correo no es válido</div>"
+        "El teléfono no es válido</div>"
     }
+    
+}
+function val_correo(){
+    var correo = document.getElementById("correo").value;
+    if (String(correo).includes('@gmail.com')){
+        document.getElementById("correo").style.border= "5px solid green";
+    }else{
+        var res = document.getElementById("correo");
+        res.style.border = "1px solid red"
+        document.getElementById("resultado").innerHTML = "<div class='alert alert-danger  mx-auto text-center'>" +
+        "El correo no es válido</div>"
+    }
+    
+}
+function val_edad(){
+    var edad = document.getElementById("edad").value;
+    if (Number(edad) >= 18){
+        document.getElementById("edad").style.border= "5px solid green";
+    }else{
+        var res = document.getElementById("edad");
+        res.style.border = "1px solid red"
+        document.getElementById("resultado").innerHTML = "<div class='alert alert-danger  mx-auto text-center'>" +
+        "debes ser mayor de edad</div>"
+    }
+    
+}
+function val_razon(){
+    var razon = document.getElementById("razon").value;
+    if(Number(razon) >= 2 && Number(razon) <= 4){
+        document.getElementById("razon").style.border= "5px solid green";
+    }else{
+        var res = document.getElementById("razon");
+        res.style.border = "1px solid red"
+        document.getElementById("resultado").innerHTML = "<div class='alert alert-danger  mx-auto text-center'>" +
+        "Seleccione una opcion valida</div>"
+    }
+    
 }
--- retro/core/static/js/validarcrear.js
+++ retro/core/static/js/validarcrear.js
@@ -1,28 +1,3 @@
-function valicuenta(){ 
-    var email = document.getElementById("email").value;
-    var pass = document.getElementById("contrasena").value;
-    if((email.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/))){
-        if(String(pass).length >= 8 && String(pass).length <= 15){
-            /* a */
-            document.getElementById("email").style.border = "1px solid lightgrey";
-            document.getElementById("contrasena").style.border = "1px solid lightgrey";
-            document.getElementById("resultado").innerHTML = "<div style='margin-top: 2%' class='alert alert-success w-50 mx-auto text-center'>" +
-            "Validado exitosamente</div>"
-            /* b */
-            document.getElementById("resultado").innetHTML = "<div style='margin-top: 2%' class='alert alert-success w-50 mx-auto text-center'>" +
-            "Cuenta creada con exito</div>"
-        } else {
-            document.getElementById("contrasena").style.border = "1px solid red";
-            document.getElementById("resultado").innerHTML = "<div style='margin-top: 2%' class='alert alert-danger w-50 mx-auto text-center'>" +
-                    "Contraseña debe tener minimo 8 caracteres</div>"
-        }
-    } else {
-        document.getElementById("email").style.border = "1px solid red"
-        document.getElementById("resultado").innerHTML = "<div style='margin-top: 2%' class='alert alert-danger w-50 mx-auto text-center'>El correo debe ser valido</div>"
-
-    }
-}
-
 function v_mail(){
     var email = document.getElementById("email").value;
     if((email.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/))){
@@ -52,5 +27,27 @@ function v_pass2(pass){
         document.getElementById("contrasena").style.border = "1px solid red";
         document.getElementById("resultado").innerHTML = "<div style='margin-top: 2%' class='alert alert-danger w-50 mx-auto text-center'>" +
         "Las contraseñas deben ser iguales</div>"
+    }
+}
+
+function validar_e() {
+    var e = document.getElementById("correo-e").value;
+
+    if (e.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)) {
+        document.getElementById("correo-e").style.border= "5px solid green";
+    } else {
+        document.getElementById("correo-e").style.border= "5px solid red";
+        document.getElementById("resultado").innerHTML= "<div class='alert alert-danger w-60 mx-auto text-center'>" +"El correo ingresado no es valido</div>";
+    }
+}
+
+function validar_c(){
+    var c = document.getElementById("clave").value;
+
+    if (String(c).length >= 8 && String(c).length <= 15) {
+        document.getElementById("clave").style.border= "5px solid green";
+    }else{
+        document.getElementById("clave").style.border= "5px solid red";
+        document.getElementById("resultado").innerHTML="<div class='alert alert-danger w-60 mx-auto text-center'>" +"La contraseña debe tener al menos 8 caracteres</div>";
     }
 }
